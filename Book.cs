fd75e550389f21f12620ca855d0598aa0663a163
Mohamed El Sayed <alsaidm508@gmail.com>
2025-01-16 21:28:11 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPTask
+{
+    public struct Book
+    {
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string ISPN { get; set; }
+
+    }
+}
